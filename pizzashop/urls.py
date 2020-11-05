c4d5f52ea1e0a21251abcba90c8b76b561511db5
SsS_JKK <kobil07@mail.ru>
2020-11-05 17:44:25 +0500

    First commit

---
+++ pizzashop/urls.py
@@ -0,0 +1,36 @@
+"""pizzashop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from pizzashopapp import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'pizzashopapp/login.html'), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page = '/'), name = 'logout'),
+    path('pizzashop/',views.pizzashop_home,name='pizzashop-home'),
+    path('pizzashop/sign-up',views.pizzashop_sign_up,name='sign-up'),
+    path('pizzashop/account',views.pizzashop_account,name='pizzashop-account'),
+    path('pizzashop/pizza',views.pizzashop_pizza,name='pizzashop-pizza'),
+    path('pizzashop/pizza/add',views.pizzashop_add_pizza,name='pizzashop-add-pizza'),
+    path('pizzashop/pizza/edit/<int:pizza_id>',views.pizzashop_edit_pizza,name='pizzashop-edit-pizza'),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
